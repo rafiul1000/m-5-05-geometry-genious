528d3b9f9be282a71c5e1ec3fb53b4f98e9bd38a
rafiul1000 <rafiulislam5075@gmail.com>
2023-08-18 12:01:18 +0600

    created geometry genious app

---
+++ js/area-calculator.js
@@ -0,0 +1,141 @@
+function calculateTriangleArea(){
+
+    // get triangle-base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText);
+    console.log(base);
+
+    // get triangle height value
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+    console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    // show triangle area
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+
+    addToCalculationEntry('Triangle', area);
+}
+
+
+function calculateRectangleArea(){
+    
+    // get rectangle width
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    console.log(width);
+
+    // get rectangle length
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    console.log(length);
+
+
+    // validate input: width & length.
+    if(isNaN(width) || isNaN(length)){
+        alert('Please insert a number.');
+        return;
+    }
+
+
+
+
+    const area = width * length;
+    console.log(area);
+
+    // show rectangle area
+    const areaSpan = document.getElementById('rectangle-area');
+    areaSpan.innerText = area;
+
+
+    addToCalculationEntry('Rectangle', area);
+}
+
+
+// reuseable function --->> reduce duplicate code
+    function calculateParallelogramArea(){
+        const base = getInputValue('parallelogram-base');
+        const height = getInputValue('parallelogram-height');
+        // validate
+        if(isNaN(base) || isNaN(height)){
+            alert('Please, insert number.');
+            return;
+        }
+        const area = base * height;
+        setElementInnerText('parallelogram-area', area);
+
+
+
+        // add to calculation
+        // add to calculation parallelogram.
+        addToCalculationEntry('parallelogram', area);
+    }
+
+
+    function calculateEllipseArea(){
+        const majorRadius = getInputValue('ellipse-major-radius');
+        const minorRadius = getInputValue('ellipse-minor-radius');
+        const area = 3.14 * majorRadius * minorRadius;
+        setElementInnerText('ellipse-area', areaToDecimal);
+
+
+        addToCalculationEntry('Ellipse', areaToDecimal); 
+    }
+
+
+    // reuseable get input value field in number
+    function getInputValue(fieldId){
+        const inputField = document.getElementById(fieldId);
+        const inputValueText = inputField.value;
+        const value = parseFloat(inputValueText);
+        return value;
+    }
+
+    // reuseable set span, div, p, etc text
+    function setElementInnerText(elementId, area){
+        const element = document.getElementById(elementId);
+        element.innerText = area;
+    }
+
+
+    // add to calculation entry:
+    /* 
+    
+    1. get the element where you want to add the dynamic HTML.
+    2. create an element you want to add.
+    3. if needed add some class.
+    4. set innerHTML. It could be dynamic Template string.
+    5. append the created element as a child of the parent.
+    
+    */
+    function addToCalculationEntry(areaType, area){
+        console.log(areaType+ ' '+ area);
+
+        const calculationEntry = document.getElementById('calculation-entry');
+
+        const count = calculationEntry.childElementCount;
+        const p = document.createElement('p');
+
+        p.classList.add('my-4');
+
+        p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class='btn btn-sm btn-success'>Convert</button>`;
+
+        calculationEntry.appendChild(p);
+    }
+
+
+    // data validation
+    /* 
+    
+    1. set the proper type of the input field. (number, data, email).
+    2. check type using typeof.
+    3. NaN means: Not a Number. isNaN is checking whether the input is not a number or not
+    
+    */
